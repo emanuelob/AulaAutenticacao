9456f32a3ead45f8e87853e092e5a59ca0f4ab4d
Emanuel de Oliveira <oliveirabar.emanuel@gmail.com>
2024-05-22 23:32:26 -0300

    :truck: chore(auth): create module, controller & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, PostModule],
+  imports: [PrismaModule, UserModule, PostModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
