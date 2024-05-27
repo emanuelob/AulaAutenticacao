08dcc3ab416df75aa6f4be1539236d298142e11e
Emanuel de Oliveira <oliveirabar.emanuel@gmail.com>
2024-05-26 22:29:43 -0300

    :sparkles: feat(auth): add login validation middleware

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
@@ -6,19 +6,22 @@ import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
-import { ConfigService } from '@nestjs/config';
+import { LoginValidationMiddleware } from './middlewares/login-validation.middleware';
 
 @Module({
-  imports: [UserModule, PassportModule, JwtModule.registerAsync({
-    imports: [],
-    inject: [ConfigService],
-    useFactory: async (configService: ConfigService) => ({
-      secret: configService.get<string>('JWT_SECRET'),
+  imports: [
+    UserModule, 
+    PassportModule, 
+    JwtModule.register({
+      secret: `${process.env.JWT_SECRET}`,
       signOptions: { expiresIn: '1d' },
-    }),
-  })],
+    })],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy]
 })
 
-export class AuthModule {}
+export class AuthModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+      consumer.apply(LoginValidationMiddleware).forRoutes('login');
+  }
+}
